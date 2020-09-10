aac12a2eeff394832aa1e5131c1328baee90a447
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-09-10 12:08:21 -0500

    cambios con row

--- src/components/peliculas/InsertarPelicula.jsx
+++ src/components/peliculas/InsertarPelicula.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import DatosUsuario from '../../context/DatosUsuario';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 
 const InsertarPelicula = () => {
     const [dataPelicula, setDataPelicula] = useState({
@@ -36,51 +36,68 @@ const InsertarPelicula = () => {
     //    console.log(contextType)
     return (
         <Form id="frm_insertar_pelicula" onSubmit={enviarPelicula}>
-            <Form.Group controlId="title">
-                <Form.Label>Titulo (*)</Form.Label>
-                <Form.Control type="text" name="title" placeholder="Titulo" defaultValue={dataPelicula.title} onChange={capturaDatosFormulario} required />
-            </Form.Group>
-
-            <Form.Group controlId="year">
-                <Form.Label>Año (*)</Form.Label>
-                <Form.Control type="number" name="year" placeholder="YYYY" min="2010" max="2020" defaultValue={dataPelicula.year} onChange={capturaDatosFormulario} required />
-            </Form.Group>
-
-            <Form.Group controlId="cover">
-                <Form.Label>Caratula (*)</Form.Label>
-                <Form.Control type="url" name="cover" placeholder="Caratula de la portada" defaultValue={dataPelicula.cover} onChange={capturaDatosFormulario} required />
-            </Form.Group>
-
-            <Form.Group controlId="description">
-                <Form.Label>Descripción</Form.Label>
-                <Form.Control as="textarea" rows="3" name="description" placeholder="Descripción de la pelicula" defaultValue={dataPelicula.description} onChange={capturaDatosFormulario} />
-            </Form.Group>
-
-            <Form.Group controlId="duration">
-                <Form.Label>Año (*)</Form.Label>
-                <Form.Control type="number" name="duration" placeholder="Duración en minutos" min="30" max="300" defaultValue={dataPelicula.duration} onChange={capturaDatosFormulario} required />
-            </Form.Group>
-
-            <Form.Group controlId="contentRating">
-                <Form.Label>Clasificación (*)</Form.Label>
-                <Form.Control as="select" name="contentRating" defaultValue={dataPelicula.contentRating} onChange={capturaDatosFormulario} required>
-                    <option value="R">Restringido</option>
-                    <option value="G">General</option>
-                    <option value="PG">En compañia de un adulto</option>
-                    <option value="PG-13">En compañia de un adulto y publico mayor a 13</option>
-                    <option value="NC-17">Publico mayor a 17</option>
-                </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="source">
-                <Form.Label>Fuente (*)</Form.Label>
-                <Form.Control type="url" name="source" placeholder="URL Fuente" defaultValue={dataPelicula.source} onChange={capturaDatosFormulario} required />
-            </Form.Group>
-
-            <Form.Group controlId="tags">
-                <Form.Label>Tags (*)</Form.Label>
-                <Form.Control type="text" name="tags" placeholder="Cada tag debe ir con una ," defaultValue={dataPelicula.tags} onChange={capturaDatosFormulario} required />
-            </Form.Group>
+            <Row>
+                <Col>
+                    <Form.Group controlId="title">
+                        <Form.Label>Titulo (*)</Form.Label>
+                        <Form.Control type="text" name="title" placeholder="Titulo" defaultValue={dataPelicula.title} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="year">
+                        <Form.Label>Año (*)</Form.Label>
+                        <Form.Control type="number" name="year" placeholder="YYYY" min="2010" max="2020" defaultValue={dataPelicula.year} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group controlId="cover">
+                        <Form.Label>Caratula (*)</Form.Label>
+                        <Form.Control type="url" name="cover" placeholder="Caratula de la portada" defaultValue={dataPelicula.cover} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="source">
+                        <Form.Label>Fuente (*)</Form.Label>
+                        <Form.Control type="url" name="source" placeholder="URL Fuente" defaultValue={dataPelicula.source} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group controlId="duration">
+                        <Form.Label>Año (*)</Form.Label>
+                        <Form.Control type="number" name="duration" placeholder="Duración en minutos" min="30" max="300" defaultValue={dataPelicula.duration} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="contentRating">
+                        <Form.Label>Clasificación (*)</Form.Label>
+                        <Form.Control as="select" name="contentRating" defaultValue={dataPelicula.contentRating} onChange={capturaDatosFormulario} required>
+                            <option value="R">Restringido</option>
+                            <option value="G">General</option>
+                            <option value="PG">En compañia de un adulto</option>
+                            <option value="PG-13">En compañia de un adulto y publico mayor a 13</option>
+                            <option value="NC-17">Publico mayor a 17</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group controlId="tags">
+                        <Form.Label>Tags (*)</Form.Label>
+                        <Form.Control type="text" name="tags" placeholder="Cada tag debe ir con una ," defaultValue={dataPelicula.tags} onChange={capturaDatosFormulario} required />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="description">
+                        <Form.Label>Descripción</Form.Label>
+                        <Form.Control as="textarea" rows="3" name="description" placeholder="Descripción de la pelicula" defaultValue={dataPelicula.description} onChange={capturaDatosFormulario} />
+                    </Form.Group>
+                </Col>
+            </Row>
             <Button variant="primary" type="submit">
                 Enviar
             </Button>
